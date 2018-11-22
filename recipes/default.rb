3d568e25ec42536878dd6580ee2e94a203fc69c7
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-11-22 15:50:30 +0000

    MAAK 1.0 : Installing logstash

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,41 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'default-jre'
+
+execute 'elasticsearch key' do
+  command 'wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -'
+end
+
+execute 'place url in sources list' do
+  command 'echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list'
+end
+
+execute "update" do
+  command 'sudo apt-get update'
+end
+
+execute "install logstash" do
+  command 'apt-get install logstash -y'
+end
+
+
+service "logstash" do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+template '/etc/logstash/jvm.options' do
+  source 'jvm.options.erb'
+  notifies :restart, 'service[logstash]'
+end
+
+template '/etc/logstash/conf.d/syslog.conf' do
+  source 'syslog.conf.erb'
+  notifies :restart, 'service[logstash]'
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,25 +11,32 @@ describe 'logstash::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it "runs apt get update" do
+      expect(chef_run).to update_apt_update 'update_sources'
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it "should install defaul-jre" do
+      expect(chef_run).to install_package 'default-jre'
+    end
+
+    it "should install logstash" do
+      expect(chef_run).to install_package 'logstash'
+    end
+
+    it "should create template jvm.options in etc/logstash/" do
+      expect(chef_run).to create_template('/etc/logstash/jvm.options')
+    end
+
+    it "should create template syslog.conf in etc/logstash/conf.d/" do
+      expect(chef_run).to create_template('/etc/logstash/conf.d/syslog.conf')
     end
   end
 end
